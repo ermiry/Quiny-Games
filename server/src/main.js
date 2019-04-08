1ac955551c87c0afe8632126aad8427713aa3d1a
ermiry <stumpestro@ermiry.com>
2019-04-08 18:00:54 -0500

    Server an client running correctly

---
+++ server/src/main.js
@@ -0,0 +1,40 @@
+const result = require ('dotenv').config ({ path: './config/keys.env' });
+// if (result.error) throw result.error
+
+const express = require ('express');
+const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+const passport = require ('passport');
+
+const app = express ();
+
+// Body parser middleware
+app.use (bodyParser.urlencoded ({ extended: false }));
+app.use (bodyParser.json());
+
+
+// DB Config
+// const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+// mongoose
+//     .connect ('mongodb://localhost/test', { useNewUrlParser: true })
+//     .then (() => console.log ('MongoDB Connected'))
+//     .catch (err => console.log (err));
+
+// passport
+// FIXME:
+// app.use (passport.initialize ());
+// require ('../config/passport')(passport);
+
+/*** ROUTES ***/
+
+app.get ("/", (req, res) => { res.json ({ msg: "Quiny Games "}); });
+
+// catch all, for now redirect to the home page
+app.get ("*", (req, res) => { res.redirect ("/"); });
+
+// const port = process.env.port;
+const port = 8080;
+
+app.listen (port, () => console.log (`Server running on port ${port}`));
