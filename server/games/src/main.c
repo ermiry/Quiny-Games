b6bd28dc89326ccdde125e434748f273cacfc96f
Erick Salas <stumpestro@ermiry.com>
2019-04-11 23:04:57 -0500

    Getting cerver ready for testing

--- server/games/src/main.c
+++ server/games/src/main.c
@@ -1,83 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 
 #include <signal.h>
-#include <pthread.h>
 
 #include "cerver.h"
-#include "network.h"
 
-#include <utils/log.h>
-#include "utils/myUtils.h"
-#include "utils/config.h"
+#include "utils/log.h"
 
-/*** THREAD ***/
+Server *main_server = NULL;
 
-// TODO: have the idea of creating many virtual servers in different sockets?
-// or maybe we can create many servers and handle the requests via a load balancer,
-// that is only listening on one port?
-// TODO: if we want to send a file, maybe create a new TCP socket in a new port?
+void close_program (int dummy);
 
-// 13/10/2018 -- the idea here is to have multiple servers that serve different purposses, for example:
-// we can have the game servers that handle the in game multiplayer logic,
-// but we can also have a server that can handle other types of requests such as getting files
-// maybe all the requests can arrive to the load balancer and depending on the request type and 
-// any other paramater that we give it, it can redirect the request to the correct server
+static int init (void) {
 
-// TODO: maybe handle this in a separate list by a name?
-Server *gameServer = NULL;
+    signal (SIGINT, close_program);
+    // TODO: load app specific data
 
-// correctly closes any on-going server and process when quitting the appplication
-void closeProgram (int dummy) {
-
-    if (gameServer) cerver_teardown (gameServer);
-    else logMsg (stdout, NO_TYPE, NO_TYPE, "There isn't any server to teardown. Quitting application.");
+    return 0;
 
 }
 
-// FIXME: how can we signal the process to end?
-// TODO: recieve signals to init, retsart or teardown a server -> like a control panel
+// TODO: destroy any app specific data
+static int end () { return 0; }
+
+// correctly closes any on-going server and exits
+void close_program (int dummy) {
+    
+    cerver_teardown (main_server);
+    end ();
+    exit (0);
+
+}
+
 int main (void) {
 
-    // TODO: if the server uses tcp, we need to listen and accept connections
-    // but if the server uses udp, we just need to recieve packets
+    if (init ()) {
+        logMsg (stderr, ERROR, NO_TYPE, "Failed to init app!");
+        return EXIT_FAILURE;
+    }
 
-    /* expected workflow: 
-    - crete server(s) with the desired type
-    - start the servers
-        - tcp servers need to accept connections and handle logic
-            - but what if we can make the game server handle different protocols?
-        - udp servers just need to handle packets
-    */
+    main_server = cerver_createServer (NULL, GAME_SERVER, "Quiny Server");
+    if (main_server) {
+        // set our own functions to authenticate our clients
+        // cerver_set_auth_method (gameServer, blackrock_authMethod);
 
-    // init other program's values...
-    signal (SIGINT, closeProgram);
+        // // set our own functions to load and delete global game data
+        // gs_set_loadGameData (gameServer, blackrock_loadGameData);
+        // gs_set_deleteGameData (gameServer, blackrock_deleteGameData);
 
-    // create a new server
-    // gameServer = cerver_createServer (NULL, GAME_SERVER, destroyGameServer);
-    // if (gameServer) {
-        // FIXME: if we have got a valid server, we are now ready to listen for connections
-        // and we can handle requests of the connected clients
+        // // set blackrock arcade game init function
+        // gs_add_gameInit (gameServer, ARCADE, blackrock_init_arcade);
 
-        // pthread_t handlerThread;
-        // if (pthread_create (&handlerThread, NULL, connectionHandler, server) != THREAD_OK)
-        //     die ("Error creating handler thread!");
+        // // set lobby game data destroy function, to delete our world and
+        // // any other data that blackrock lobby uses
+        // gs_set_lobbyDeleteGameData (gameServer, deleteBrGameData);
 
-        // listenForConnections (server);
+        // if (cerver_startServer (gameServer)) 
+        //     logMsg (stderr, ERROR, SERVER, "Failed to start server!");
+    } 
 
-        // // at this point we are ready to listen for connections...
-        // logMsg (stdout, SERVER, NO_TYPE, "Waiting for connections...");
-        // // TODO: we need to tell our game server to listen for connections
-        // listenForConnections (server);
+    else logMsg (stderr, ERROR, NO_TYPE, "Failed to create Quiny Server!");
 
-        // startServer (gameServer);
-    // }
-
-    // if we reach this point, be sure to correctly clean all of our data...
-    // closeProgram (0);
-    
-    return 0;
+    return end ();
 
 }
