594027ea7ab1f3adb346513b43fb23a88bde5b15
ermiry <stumpestro@ermiry.com>
2019-04-20 03:05:53 -0500

    Split lobby into its own file

--- server/games/include/cerver/game/game.h
+++ server/games/include/cerver/game/game.h
@@ -7,6 +7,7 @@
 
 #include "cerver/cerver.h"
 #include "player.h"
+#include "lobby.h"
 
 #include "collections/dllist.h"
 #include "collections/avl.h"
@@ -74,51 +75,6 @@ typedef enum GameType {
 	ARCADE = 1
 
 } GameType;
-
-struct _GameSettings {
-
-	GameType gameType;
-
-	u8 playerTimeout; 	// in seconds.
-	u8 fps;
-
-	u8 minPlayers;
-	u8 maxPlayers;
-
-	// duration?
-
-};
-
-typedef struct _GameSettings GameSettings;
-
-struct _Lobby {
-
-	GameSettings *settings;
-	bool inGame;
-
-	Player *owner;				// the client that created the lobby -> he has higher privileges
-
-	bool isRunning;				// lobby is listening for player packets
-
-	// 04/11/2018 -- lets try this and see how it goes - intended to also work for a bigger 
-	// lobby with more players in it
-	AVLTree *players;							// players inside the lobby
-    struct pollfd players_fds[4];     			// 04/11/2018 - 4 max players in lobby
-    u16 players_nfds;                           // n of active fds in the pollfd array
-    bool compress_players;              		// compress the fds array?
-    u32 pollTimeout;    
-
-	// the server admin can add its server specific data types
-	void *gameData;
-	Action deleteLobbyGameData;
-
-	// 21/11/2018 - we put this here to avoid race conditions if we put it on
-	// the server game data
-	Pool *gamePacketsPool;
-
-};
-
-typedef struct _Lobby Lobby;
 
 // 10/11/2018 - aux reference to a server and lobby for thread functions
 typedef struct ServerLobby {
