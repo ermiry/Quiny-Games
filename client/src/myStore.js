38239c630ae4504d83c55b25e580c62559eb972b
Erick Salas <stumpestro@ermiry.com>
2019-04-24 12:42:23 -0500

    Testing production env

--- client/src/myStore.js
+++ client/src/myStore.js
@@ -13,7 +13,8 @@ const middleware = [thunk];
 const myStore = createStore (
     rootReducer, 
     initialState, 
-    compose (applyMiddleware (...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ ()));
+    compose (applyMiddleware (...middleware))
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ ())
+);
 
 export default myStore;
