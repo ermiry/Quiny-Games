d6319b5a6029d39578d027e7313bb85baaf57abb
Erick Salas <stumpestro@ermiry.com>
2019-04-24 18:03:08 -0500

    Removed dependencies

--- server/src/routes/api/quiny.js
+++ server/src/routes/api/quiny.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router ();
-const ejs = require('ejs');
-var request = require("superagent");
 
 router.get('/test', (req,res)=>{
     var accessToken = req.query.token;
@@ -10,33 +8,33 @@ router.get('/test', (req,res)=>{
     
 })
 
-router.get('/oauth',(req,res)=>{
-  console.log("request to oauth");
-  //var accessTokenAlexa = req.query.token;
-  //var accessTokenAmazon = req.query.access_token;
-  var code = req.query.code;
-  request.post('https://dev-qpzoqcez.auth0.com/oauth/token')
-  .send(
-    {
-      "grant_type":"authorization_code",
-      "code":code,
-      "client_id":"ek5yWI1JyCXb78obHKzyfpdR14pISo0S",
-      "client_secret":"3xUciwo_7iMhyqGwa3BTi1I71sZTzyxsKgo1mkV7UDKc3Afis17c6qN_qibEQjum",
-      "redirect_uri": "https://9478a70d.ngrok.io/api/quiny/welcome"
-    })
-  .then(result => {
-      console.log(body)
-      return body;
-  })
-  .catch(error=>{
-    console.log("error")
-  })
+// router.get('/oauth',(req,res)=>{
+//   console.log("request to oauth");
+//   //var accessTokenAlexa = req.query.token;
+//   //var accessTokenAmazon = req.query.access_token;
+//   var code = req.query.code;
+//   request.post('https://dev-qpzoqcez.auth0.com/oauth/token')
+//   .send(
+//     {
+//       "grant_type":"authorization_code",
+//       "code":code,
+//       "client_id":"ek5yWI1JyCXb78obHKzyfpdR14pISo0S",
+//       "client_secret":"3xUciwo_7iMhyqGwa3BTi1I71sZTzyxsKgo1mkV7UDKc3Afis17c6qN_qibEQjum",
+//       "redirect_uri": "https://9478a70d.ngrok.io/api/quiny/welcome"
+//     })
+//   .then(result => {
+//       console.log(body)
+//       return body;
+//   })
+//   .catch(error=>{
+//     console.log("error")
+//   })
 
-  //var info = amazon.getProfile(accessTokenAmazon);
+//   //var info = amazon.getProfile(accessTokenAmazon);
   
-  //console.log(info)
-  res.render('login',{"token":req.query.access_token});
-})
+//   //console.log(info)
+//   res.render('login',{"token":req.query.access_token});
+// })
 
 router.get("/welcome",(req,res)=>{
   console.log("welcome");
