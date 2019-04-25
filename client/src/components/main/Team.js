aac32bc4461d0e69476f858732b3966052708bdb
Erick Salas <stumpestro@ermiry.com>
2019-04-25 04:30:58 -0500

    Added team view

--- client/src/components/main/Team.js
+++ client/src/components/main/Team.js
@@ -4,8 +4,44 @@ class Team extends Component {
   
     render () {
         return (
-            <div>
-                <h1>Team</h1>
+            <div className="team">
+                <div className="container">
+                <h1 className="heading">Our Team</h1>
+                    <br/>
+                    <div className="card-wrapper">
+                        <div className="card"> 
+                            <img src="img//ArturoCasillas.jpeg" alt="ArturoCasillas" className="profile-img"/>
+                            <h1>Casillas Arutro</h1>
+                            <p className="job-title">APL Developer</p>
+                            <p className="about">Programmer of Alexa Multimodels Skill and Interactions </p>
+                        </div>
+                        <div className="card"> 
+                            <img src="img//LuisHuerta.jpeg" alt="LuisHuerta" className="profile-img"/>
+                            <h1>Huerta Luis</h1>
+                            <p className="job-title">Data Base Manager</p>
+                            <p className="about">Designer and manager of the data base of Quiny Games</p>
+                        </div>
+                        <div className="card"> 
+                            <img src="img//JuanLara.jpeg" alt="JuanLara" className="profile-img"/>
+                            <h1>Lara Juan Carlos</h1>
+                            <p className="job-title">Alexa Skill Expert</p>
+                            <p className="about">Programmer of the skill of Quiny Games</p>
+                        </div>
+                        <div className="card"> 
+                            <img src="img//KarenPonce.jpeg" alt="KarenPonce" className="profile-img"/>
+                            <h1>Ponce Karen</h1>
+                            <p className="job-title">Frontend Developer</p>
+                            <p className="about">Programmer and designer of the frontend of Quiny Games</p>
+                        </div>
+                        <div className="card"> 
+                            <img src="img//ErickSalas.jpeg" alt="Erick" className="profile-img"/>
+                            <h1>Salas Erick</h1>
+                            <p className="job-title">Leading Developer</p>
+                            <p className="about">Wrote a custome server framework in C and developer of the Quiny Games API that connects the frontend with the backend</p>
+                        </div>
+                    </div>
+                    <br/>
+                </div>
             </div>
         );
     }
--- server/src/routes/api/quiny.js
+++ server/src/routes/api/quiny.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router ();
+const ejs = require('ejs');
+
+const auth0 = require ('auth0-js');
+const reqq = require ('axios');
+
+var token;
+var userProfile;
+
 
 router.get('/test', (req,res)=>{
     var accessToken = req.query.token;
@@ -8,33 +16,56 @@ router.get('/test', (req,res)=>{
     
 })
 
-// router.get('/oauth',(req,res)=>{
-//   console.log("request to oauth");
-//   //var accessTokenAlexa = req.query.token;
-//   //var accessTokenAmazon = req.query.access_token;
-//   var code = req.query.code;
-//   request.post('https://dev-qpzoqcez.auth0.com/oauth/token')
-//   .send(
-//     {
-//       "grant_type":"authorization_code",
-//       "code":code,
-//       "client_id":"ek5yWI1JyCXb78obHKzyfpdR14pISo0S",
-//       "client_secret":"3xUciwo_7iMhyqGwa3BTi1I71sZTzyxsKgo1mkV7UDKc3Afis17c6qN_qibEQjum",
-//       "redirect_uri": "https://9478a70d.ngrok.io/api/quiny/welcome"
-//     })
-//   .then(result => {
-//       console.log(body)
-//       return body;
-//   })
-//   .catch(error=>{
-//     console.log("error")
-//   })
-
-//   //var info = amazon.getProfile(accessTokenAmazon);
+router.get ('/oauth',(req,res)=>{
+  console.log("request to oauth");
+  //var accessTokenAlexa = req.query.token;
+  //var accessTokenAmazon = req.query.access_token;
+  var code = req.query.code;
+  request.post('https://dev-qpzoqcez.auth0.com/oauth/token')
+  .send(
+    {
+      "grant_type":"authorization_code",
+      "code":code,
+      "client_id":"ek5yWI1JyCXb78obHKzyfpdR14pISo0S",
+      "client_secret":"3xUciwo_7iMhyqGwa3BTi1I71sZTzyxsKgo1mkV7UDKc3Afis17c6qN_qibEQjum",
+      "redirect_uri": "https://9478a70d.ngrok.io/api/quiny/welcome"
+    })
+  .then(result => {
+    token = result.body.access_token;
+      console.log(result.body.access_token  +"\n tokenID: " + result.body.id_token)
+      
+      //return result;
+  })
+  .catch(error=>{
+    console.error(error);
+    console.log("error")
+  })
+  //var info = amazon.getProfile(accessTokenAmazon);
   
-//   //console.log(info)
-//   res.render('login',{"token":req.query.access_token});
-// })
+  //console.log(info)
+  res.render('login',{"token":req.query.access_token});
+});
+
+// FIXME: call this after we recieve the token
+router.get('/profile',(req,res)=>{
+  var config = {
+    headers:{'Authorization': 'Bearer ' + token}
+  }
+  console.log(token);
+  
+  reqq.default.get("https://dev-qpzoqcez.auth0.com/userinfo",config)
+  .then(result=>{
+    console.log(result);
+    // TODO: get the user email and use it to add the user to add the token to the 
+    // player info in the cerver
+
+    // redirect back to react
+  })
+  .catch(error=>{
+    console.error("error" + error.status);
+  })
+});
+
 
 router.get("/welcome",(req,res)=>{
   console.log("welcome");
@@ -51,9 +82,6 @@ router.get('/start',(req,res)=>{
         res.send(info);
 })
 
-router.get('/assets/startAudio',(req,res)=>{
-    
-})
 
 
 router.get('/firstQuestion',(req,res)=>{
@@ -81,7 +109,7 @@ router.post('/answer',(req,res)=>{
 })
 
 router.get('/getNextQuestion',(req,res)=>{
-  
+
   
 
     console.log("in next question + \n" + 
@@ -93,6 +121,7 @@ router.get('/getNextQuestion',(req,res)=>{
 })
 
 router.get('/views/apl',(req,res)=>{
+  console.log("Pruea");
     var apl = {
         "type": "APL",
         "version": "1.0",
